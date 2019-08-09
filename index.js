7d46236ca3bc02771fd39ed81eb3f1a126f17bfc
Chip Wasson <chip@wasson.io>
2019-08-08 22:05:11 -0600

    Add prometheus metrics endpoint

--- index.js
+++ index.js
@@ -12,15 +12,35 @@ const config = {
     host: process.env.REDIS_HOST || "localhost",
     port: process.env.REDIS_PORT || 6379,
     db: process.env.REDIS_DB || 0
-  }
+  },
+  countKey: process.env.COUNT_KEY || "count",
+  metricCountKey: process.env.COUNT_KEY || "metric-count"
 };
 
 const app = express();
 
 client = redis.createClient({ ...config.redis });
 
+const getAsync = async (client, name) => {
+  return new Promise((resolve, reject) => {
+    client.get(name, (e, r) => {
+      if (e) reject(e);
+      resolve(r);
+    });
+  });
+};
+
+const incrbyAsync = async (client, name, count) => {
+  return new Promise((resolve, reject) => {
+    client.incrby(name, count, (e, r) => {
+      if (e) reject(e);
+      resolve(r);
+    });
+  });
+};
+
 app.get("/", (req, res) => {
-  client.incrby("count", 1, (e, r) => {
+  client.incrby(config.countKey, 1, (e, r) => {
     if (e) {
       console.error(e);
       res.send("Oh hooray, the app is broken! ╮( ˘ ､ ˘ )╭");
@@ -31,12 +51,30 @@ app.get("/", (req, res) => {
 });
 
 app.get("/meta", (req, res) => {
-  const stats = [`hostname: ${os.hostname()}`];
+  const stats = [`hostname: ${os.hostname()}`, `domain: ${req.hostname}`];
   res.send(
     [
       "Ooooooo I know how to find new paths and see meta data! pshaw.",
       ...stats
     ].join("<br/>")
+  );
+});
+
+app.get("/metrics", async (req, res) => {
+  const count = await getAsync(client, config.countKey);
+  const metricCount = await incrbyAsync(client, config.metricCountKey, 1);
+  const metrics = [
+    `count{domain="${req.hostname}", host="${os.hostname()}"} ${count}`,
+    `metricCount{domain="${
+      req.hostname
+    }", host="${os.hostname()}"} ${metricCount}`
+  ];
+  res.setHeader("content-type", "text/plain");
+  res.send(
+    [
+      "# oh well beep boop, look at mister bigshot robot over here, scraping metrics!",
+      ...metrics
+    ].join("\n")
   );
 });
 
